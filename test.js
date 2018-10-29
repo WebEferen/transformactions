2f3872688b196642673a22717a6d727a30ca99e8
WebEferen <michal.makowski97@gmail.com>
2018-10-29 11:17:47 +0100

    Enabled test case

--- test.js
+++ test.js
@@ -29,17 +29,17 @@ transformactions.converter.convert({
   withHeaders: true
 }).then(() => console.log('Converted Successfully!'));
 
-// transformactions.parser.parse('test.xls', {
-//   input: {
-//     // headerLine: 0,
-//     contentStartsAt: 1,
-//     delimiter: ';',
-//     newLineDelimiter: '\n',
-//     sheetName: ''
-//   },
-//   headerKeys: true,
-//   autoHeaders: false,
-// }).then((res) => console.log(res));
+transformactions.parser.parse('test.xls', {
+  input: {
+    // headerLine: 0,
+    contentStartsAt: 1,
+    delimiter: ';',
+    newLineDelimiter: '\n',
+    sheetName: ''
+  },
+  headerKeys: true,
+  autoHeaders: false,
+}).then((res) => console.log(res));
 
 // CSV parsing
 
